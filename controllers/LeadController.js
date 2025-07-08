ea8fd09fdbfe2ae17de08039a1700d87807ac6ff
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-08 17:14:55 +0530

    chore: ignore sensitive log and env files

--- controllers/LeadController.js
+++ controllers/LeadController.js
@@ -2,36 +2,39 @@ import { successResponse, errorResponse } from '../helpers/ResponseHandler.js';
 import axios from 'axios';
 import fs from 'fs';
 import path from 'path';
+import Settings from '../models/Settings.js';
 
 export const create = async (req, res) => {
-    console.log("Here");
     const logPath = path.join(process.cwd(), 'storage', 'backend.log');
     const logData = `[${new Date().toISOString()}] ${JSON.stringify(req.body)}\n`;
     fs.mkdirSync(path.dirname(logPath), { recursive: true });
+    fs.appendFileSync(logPath, logData);
     
     try{
-        const { token, url } = req.salesforce;
+        const settings = await Settings.findOne();
+        const { sf_access_token:token, sf_instance_url:url } = settings;
+        // const { token, url } = req.salesforce;
     
         const payload = {
-            Name      : req.body.addresses.name,
+            // Name      : req.body.addresses.name,
             FirstName : req.body.first_name, 
             LastName  : req.body.last_name,
-            Company   : req.body.addresses.company,
+            Company   : req.body.addresses?.[0]?.company,
             Email     : req.body.email,
-            Phone     : req.body.phone,
-            Description: req.body.id,
+            Phone     : req.body.phone || req.body.addresses?.[0]?.phone || '',
+            Description: `Shopify ID: ${req.body.id}`,
             Status    : "Open - Not Contacted",
             LeadSource: "Web"
         };
 
         // const payload = {
         //     // Name      : 'A',
-        //     FirstName : 'B', 
-        //     LastName  : 'C',
-        //     Company   : 'D',
-        //     Email     : 'abc@gmail.com',
-        //     Phone     : '123853',
-        //     Description: 'DE@##33423',
+        //     FirstName : 'B1', 
+        //     LastName  : 'C1',
+        //     Company   : 'D1',
+        //     Email     : 'abc1@gmail.com',
+        //     Phone     : '1238531',
+        //     Description: 'DE@##313423',
         //     Status    : "Open - Not Contacted",
         //     LeadSource: "Web"
         // };
