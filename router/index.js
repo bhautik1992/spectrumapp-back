598880966858200f43a0dfd54f9d2a5102a0dbcc
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-10 13:12:07 +0530

    change in route

--- router/index.js
+++ router/index.js
@@ -9,7 +9,7 @@ router.use("/login", Login);
 router.use("/user", User);
 router.use("/settings", Settings);
 router.use("/lead", Lead);
-router.use("/create-customer", Lead);
+// router.use("/create-customer", Lead);
 
 export default router;
 
