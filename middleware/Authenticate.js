4dfec49516eb998f131d07c90abb0c85dd347e35
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-15 18:43:06 +0530

    add file

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -2,8 +2,8 @@ import { verifyToken } from '../helpers/JWTToken.js';
 import { errorResponse } from '../helpers/ResponseHandler.js';
 
 export const protectRoute = (req, res, next) => {
-    // next();
-
+    next();
+    
     const token = req.headers['authorization']?.split(' ')[1];
 
     if(!token){
