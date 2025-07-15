460904ec94a4637e50da2e967b6d963cb3c83f8d
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-15 20:01:24 +0530

    add file

--- webhookRoutes.js
+++ webhookRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.use('/webhooks/customers-create',express.raw({ type: 'application/json' }));
 router.use('/webhooks/customers-update',express.raw({ type: 'application/json' }));
 
-router.post('/webhooks/customers-create',salesforceAuth,async (req, res) => {
+router.post('/webhooks/customers-create',async (req, res) => {
     const body = req.body.toString();
     
     const customer = JSON.parse(body);
@@ -18,7 +18,7 @@ router.post('/webhooks/customers-create',salesforceAuth,async (req, res) => {
     res.status(200).send('Customer Create Webhook Received');
 });
 
-router.post('/webhooks/customers-update',salesforceAuth,async (req, res) => {
+router.post('/webhooks/customers-update',async (req, res) => {
     const body = req.body.toString();
     
     const customer = JSON.parse(body);
