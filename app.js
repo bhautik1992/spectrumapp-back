50350d8c39e28ab20cddd1948c981d77696107ba
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-16 14:04:50 +0530

    add file

--- app.js
+++ app.js
@@ -6,15 +6,10 @@ import connectDB from './config/database.js';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { storeLog } from './helpers/Common.js';
 
 dotenv.config();
 connectDB();
-
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 app.use(cors({
     // origin: '*',
@@ -23,24 +18,19 @@ app.use(cors({
     credentials: true,
 }));
 
-// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-
-storeLog("Inside app.js");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/webhooks/customers-create', express.raw({ type: 'application/json' }));
 app.use('/webhooks/customers-update', express.raw({ type: 'application/json' }));
 
 app.use('/',webhooks); 
-
 app.use(express.json());
-
 app.use(process.env.API_PREFIX, router); 
 
 // Error handling middleware (optional)
 app.use((err, req, res, next) => {
-    storeLog(err.stack);
-    
     console.error(err.stack);
     res.status(500).json({ message: 'An error occurred', error: err.message });
 });
