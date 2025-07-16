a54971b2e4a7a0bb987ec585f71572b13a97ac04
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-16 16:22:15 +0530

    add file

--- database/schemas/CustomersSchema.js
+++ database/schemas/CustomersSchema.js
@@ -22,7 +22,7 @@ const schema = new mongoose.Schema({
     lead_source: {
         type: Number,
         required: true,
-        enum: [1, 2, 3, 4, 5],
+        enum: [1, 2, 3, 4, 5, 6],
         default: 1,
         description: '1 = Web, 2 = Phone Inquiry, 3 = Partner - Referral, 4 = Purchased - List, 5 = Other, 6 = Shopify Registration' 
     },
