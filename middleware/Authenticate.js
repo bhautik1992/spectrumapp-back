567a5183a428b0ecef6459ca6f3d99ae276800dc
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-07-16 12:08:40 +0530

    add file

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -4,19 +4,19 @@ import { errorResponse } from '../helpers/ResponseHandler.js';
 export const protectRoute = (req, res, next) => {
     next();
     
-    const token = req.headers['authorization']?.split(' ')[1];
+    // const token = req.headers['authorization']?.split(' ')[1];
 
-    if(!token){
-        return errorResponse(res, 'No token provided', null, 403);
-    }
+    // if(!token){
+    //     return errorResponse(res, 'No token provided', null, 403);
+    // }
 
-    const decoded = verifyToken(token);
-    if(!decoded){
-        return errorResponse(res, 'Invalid or expired token', null, 401);
-    }
+    // const decoded = verifyToken(token);
+    // if(!decoded){
+    //     return errorResponse(res, 'Invalid or expired token', null, 401);
+    // }
 
-    req.user = decoded;
-    next();
+    // req.user = decoded;
+    // next();
 };
 
 
--- webhookRoutes.js
+++ webhookRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { handleCustomerForSalesforce } from './utils/handleCustomerForSalesforce.js';
-import { salesforceAuth } from './middleware/salesforceAuth.js';
 import { storeLog } from './helpers/Common.js';
 
 const router = express.Router();
@@ -9,20 +8,20 @@ router.use('/webhooks/customers-create',express.raw({ type: 'application/json' }
 router.use('/webhooks/customers-update',express.raw({ type: 'application/json' }));
 
 router.post('/webhooks/customers-create',async (req, res) => {
-    storeLog("Webhook webhooks/customers-create Call")
     const body = req.body.toString();
     
     const customer = JSON.parse(body);
+    storeLog("Webhook webhooks/customers-create Call")
     await handleCustomerForSalesforce(customer);
 
     res.status(200).send('Customer Create Webhook Received');
 });
 
 router.post('/webhooks/customers-update',async (req, res) => {
-    storeLog("Webhook webhooks/customers-update Call")
     const body = req.body.toString();
     
     const customer = JSON.parse(body);
+    storeLog("Webhook webhooks/customers-update Call")
     await handleCustomerForSalesforce(customer);
 
     res.status(200).send('Customer Update Webhook Received');
