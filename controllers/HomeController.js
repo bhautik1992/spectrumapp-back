33c6bf46e0f1b40f0d66ca4488d0b3ae7e0ddc0a
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-09-15 13:11:27 +0530

    add file

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,8 +1,12 @@
 import { successResponse, errorResponse } from '../helpers/ResponseHandler.js';
 import Customers from "../models/Customers.js";
+import Events from "../models/Events.js";
 
 export const index = async (req, res) => {
     try {
+        const now = new Date();
+        const { uid } = req.params;
+
         const result = await Customers.aggregate([
             {
                 $group: {
@@ -12,7 +16,14 @@ export const index = async (req, res) => {
             }
         ]);
 
-        return successResponse(res, result);
+        const events = await Events.find({ user_id: uid, date: { $gte: now }, deletedAt: null })
+            .populate({
+                path: 'customer_id',
+                select: 'lead_first_name lead_last_name lead_email lead_phone lead_status'
+            })
+            .sort({ date: 1 });
+
+        return successResponse(res, {result,events});
     } catch (error) {
         // console.log( error.response?.data || error.message);
         return errorResponse(res, process.env.ERROR_MSG, 500);
--- router/routes/Home.js
+++ router/routes/Home.js
@@ -3,7 +3,7 @@ import { index } from '../../controllers/HomeController.js';
 import { protectRoute } from '../../middleware/Authenticate.js';
 
 const router = express.Router();
-router.route('/').get(protectRoute, index);
+router.route('/:uid').get(protectRoute, index);
 
 export default router;
 
