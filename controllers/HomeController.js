efbb7ae27e648186627190691a3b43ecb27f3828
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-09-15 16:40:53 +0530

    add file

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -5,6 +5,7 @@ import Events from "../models/Events.js";
 export const index = async (req, res) => {
     try {
         const now = new Date();
+        const startOfTodayUTC = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate()));
         const { uid } = req.params;
 
         const result = await Customers.aggregate([
@@ -16,7 +17,7 @@ export const index = async (req, res) => {
             }
         ]);
 
-        const events = await Events.find({ user_id: uid, date: { $gte: now }, deletedAt: null })
+        const events = await Events.find({ user_id: uid, date: { $gte: startOfTodayUTC }, deletedAt: null })
             .populate({
                 path: 'customer_id',
                 select: 'lead_first_name lead_last_name lead_email lead_phone lead_status'
