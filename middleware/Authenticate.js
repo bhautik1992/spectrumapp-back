85f568d4920fa2c7647b6ad6aec0c34edc11d210
bhautik1992 <bhautik.hailysoft@gmail.com>
2025-10-24 14:39:36 +0530

    add file

--- middleware/Authenticate.js
+++ middleware/Authenticate.js
@@ -5,12 +5,12 @@ export const protectRoute = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
 
     if(!token){
-        return errorResponse(res, 'No token provided', null, 403);
+        return errorResponse(res, 'No token provided', 403);
     }
 
     const decoded = verifyToken(token);
     if(!decoded){
-        return errorResponse(res, 'Invalid or expired token', null, 401);
+        return errorResponse(res, 'Invalid or expired token', 401);
     }
 
     req.user = decoded;
